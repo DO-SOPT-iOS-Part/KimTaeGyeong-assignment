6e73802b6c0c6f5334b5e00fad3dc9e92fbbac80
timotheekim10 <timotheekim10@gmail.com>
2023-10-20 04:47:41 +0900

    design: SearchController 추가

--- apple-weather-app-clone/apple-weather-app-clone/ViewController.swift
+++ apple-weather-app-clone/apple-weather-app-clone/ViewController.swift
@@ -17,12 +17,38 @@ class ViewController: UIViewController {
         return button
     }()
     
+    private let locationSearchController: UISearchController = {
+        let searchController = UISearchController()
+        searchController.searchBar.searchTextField
+            .attributedPlaceholder = NSAttributedString(string: "도시 또는 공항 검색", attributes: [NSAttributedString.Key.foregroundColor : #colorLiteral(red: 0.6178889275, green: 0.6178889275, blue: 0.6178889275, alpha: 1)])
+        searchController.searchBar.searchTextField.backgroundColor = #colorLiteral(red: 0.1353607476, green: 0.1353607476, blue: 0.1353607476, alpha: 1)
+        searchController.searchBar.searchTextField.textColor = .white
+        searchController.searchBar.tintColor = .white
+        searchController.searchBar.setValue("취소", forKey: "cancelButtonText")
+        searchController.hidesNavigationBarDuringPresentation = true
+        return searchController
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = .black
         self.navigationItem.rightBarButtonItem = rightBarButtonItem
         self.navigationItem.title = "날씨"
         self.navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
         self.navigationController?.navigationBar.prefersLargeTitles = true
+        self.setSearchController()
+        
+    }
+    
+}
+
+extension ViewController {
+    
+    private func setSearchController() {
+        self.navigationItem.searchController = locationSearchController
+        self.navigationItem.hidesSearchBarWhenScrolling = true
+        self.locationSearchController.searchBar.searchTextField.textColor = .white
+        self.locationSearchController.searchBar.searchTextField.leftView?.tintColor = #colorLiteral(red: 0.6178889275, green: 0.6178889275, blue: 0.6178889275, alpha: 1)
     }
     
 }
